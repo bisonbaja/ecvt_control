c9edc02aabcf8d74b0ac879929b5d752e6eaee10
cwilson <cwil@tutanota.com>
2024-11-10 18:37:14 -0600

    changes

--- outline.md
+++ outline.md
@@ -1,4 +1,4 @@
 Controllers:
  - Primary Controller:
     Takes engine and wheel RPM input as analog voltage, computes new sheave position. 
-    Needs 
+    
--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,20 @@
 #include <Arduino.h>
 #include <SD.h>
 #include <CAN.h>
+#define ENGINE_TACH_PIN     A1
+#define SECONDARY_TACH_PIN  A2
+#define TACH_SCALE          2475/128
 
-unsigned int 
+// Control EQN:
+// P = 0.75 - (Ka / (Rt+1)) + Kp E + Kd dE/dt + Ki integ(E)
+// Exlicit term, proportional, derivative, integral
+
+double erpmMeasured;
+double erpmTarget;
+double srpmMeasured;
+double ratioTarget;
+double ratioMeasured;
+
+void update() {
+
+}
